739e7d1b96651f018dfe93a534d7ce9af9b493b3
Matt Moore (via sockpuppet) <mattmoor+sockpuppet@google.com>
2019-07-01 14:00:54 +0000

    golang format tools
    Produced via:
      `gofmt -s -w $(find -path './vendor' -prune -o -type f -name '*.go' -print))`
      `goimports -w $(find -name '*.go' | grep -v vendor)`
    /assign @n3wscott

--- pkg/pubsub/adapter/push_test.go
+++ pkg/pubsub/adapter/push_test.go
@@ -1,10 +1,11 @@
 package adapter
 
 import (
-	"cloud.google.com/go/pubsub"
 	"context"
-	"github.com/google/go-cmp/cmp"
 	"testing"
+
+	"cloud.google.com/go/pubsub"
+	"github.com/google/go-cmp/cmp"
 
 	cloudevents "github.com/cloudevents/sdk-go"
 	cepubsub "github.com/cloudevents/sdk-go/pkg/cloudevents/transport/pubsub"
