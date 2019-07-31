921702a2dad0340043ee35a05059eb6e1823cb71
Scott Nichols <nicholss@google.com>
2019-07-31 07:57:41 -0700

    use generated namespace names.

---
+++ test/e2e/README.md
@@ -0,0 +1,6 @@
+# E2E Tests
+
+Prow will run `./e2e-tests.sh`. 
+
+## To run manually using `go test` 
+
--- test/e2e/main.go
+++ test/e2e/main.go
@@ -1,23 +1,45 @@
+/*
+Copyright 2019 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package e2etest
 
 import (
 	"fmt"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"os"
 	"os/signal"
 	"syscall"
 	"testing"
+	"time"
 
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/dynamic"
 	"knative.dev/pkg/test"
 	pkgTest "knative.dev/pkg/test"
+	"knative.dev/pkg/test/helpers"
 )
 
 // Setup creates the client objects needed in the e2e tests,
 // and does other setups, like creating namespaces, set the test case to run in parallel, etc.
 func Setup(t *testing.T, runInParallel bool) *Client {
 	// Create a new namespace to run this test case.
-	//baseFuncName := helpers.GetBaseFuncName(t.Name())
-	namespace := "default" //helpers.MakeK8sNamePrefix(baseFuncName)
+	baseFuncName := helpers.GetBaseFuncName(t.Name())
+	namespace := helpers.MakeK8sNamePrefix(baseFuncName)
 	t.Logf("namespace is : %q", namespace)
 	client, err := NewClient(
 		pkgTest.Flags.Kubeconfig,
@@ -28,7 +50,7 @@ func Setup(t *testing.T, runInParallel bool) *Client {
 		t.Fatalf("Couldn't initialize clients: %v", err)
 	}
 
-	//CreateNamespaceIfNeeded(t, client, namespace)
+	CreateNamespaceIfNeeded(t, client, namespace)
 
 	// Disallow manually interrupting the tests.
 	// TODO(Fredy-Z): t.Skip() can only be called on its own goroutine.
@@ -50,10 +72,18 @@ func Setup(t *testing.T, runInParallel bool) *Client {
 
 // TearDown will delete created names using clients.
 func TearDown(client *Client) {
-	//client.Tracker.Clean(true)
-	//if err := DeleteNameSpace(client); err != nil {
-	//	client.T.Logf("Could not delete the namespace %q: %v", client.Namespace, err)
-	//}
+	if err := DeleteNameSpace(client); err != nil {
+		client.T.Logf("Could not delete the namespace %q: %v", client.Namespace, err)
+	}
+}
+
+// DeleteNameSpace deletes the namespace that has the given name.
+func DeleteNameSpace(client *Client) error {
+	_, err := client.Kube.Kube.CoreV1().Namespaces().Get(client.Namespace, metav1.GetOptions{})
+	if err == nil || !errors.IsNotFound(err) {
+		return client.Kube.Kube.CoreV1().Namespaces().Delete(client.Namespace, nil)
+	}
+	return err
 }
 
 // Client holds instances of interfaces for making requests to Knative.
@@ -86,4 +116,36 @@ func NewClient(configPath string, clusterName string, namespace string, t *testi
 	client.Namespace = namespace
 	client.T = t
 	return client, nil
+}
+
+// CreateNamespaceIfNeeded creates a new namespace if it does not exist.
+func CreateNamespaceIfNeeded(t *testing.T, client *Client, namespace string) {
+	nsSpec, err := client.Kube.Kube.CoreV1().Namespaces().Get(namespace, metav1.GetOptions{})
+
+	if err != nil && errors.IsNotFound(err) {
+		nsSpec = &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespace}}
+		nsSpec, err = client.Kube.Kube.CoreV1().Namespaces().Create(nsSpec)
+
+		if err != nil {
+			t.Fatalf("Failed to create Namespace: %s; %v", namespace, err)
+		}
+
+		// https://github.com/kubernetes/kubernetes/issues/66689
+		// We can only start creating pods after the default ServiceAccount is created by the kube-controller-manager.
+		err = waitForServiceAccountExists(t, client, "default", namespace)
+		if err != nil {
+			t.Fatalf("The default ServiceAccount was not created for the Namespace: %s", namespace)
+		}
+	}
+}
+
+// waitForServiceAccountExists waits until the ServiceAccount exists.
+func waitForServiceAccountExists(t *testing.T, client *Client, name, namespace string) error {
+	return wait.PollImmediate(100*time.Millisecond, 1*time.Minute, func() (bool, error) {
+		sas := client.Kube.Kube.CoreV1().ServiceAccounts(namespace)
+		if _, err := sas.Get(name, metav1.GetOptions{}); err == nil {
+			return true, nil
+		}
+		return false, nil
+	})
 }
