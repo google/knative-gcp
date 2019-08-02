8749963ed8b32c0c06aded10eaec7927a49a6327
Scott Nichols <nicholss@google.com>
2019-08-02 09:41:47 -0700

    no need to search for mains, they are in cmd.

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -29,9 +29,7 @@ dep ensure
 rm -rf $(find vendor/ -name 'BUILD')
 rm -rf $(find vendor/ -name 'BUILD.bazel')
 
-update_licenses third_party/VENDOR-LICENSE \
-  $(find . -name "*.go" | grep -v vendor | xargs grep "package main" | cut -d: -f1 | xargs -n1 dirname | uniq)
-
+update_licenses third_party/VENDOR-LICENSE "./cmd/*"
 
 # HACK HACK HACK
 # TODO(https://github.com/knative/eventing/issues/1065): remove when we can update top 1.13.0 k8s clients.
