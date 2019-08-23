dd4ec8ea94704e575d81839870bbcfbf2fbdb554
Ville Aikas <vaikas@google.com>
2019-08-23 15:38:41 +0300

    remove unused imports

--- pkg/reconciler/testing/storage.go
+++ pkg/reconciler/testing/storage.go
@@ -18,11 +18,9 @@ package testing
 
 import (
 	"context"
-	//	"time"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	//	"k8s.io/apimachinery/pkg/types"
 
 	"knative.dev/pkg/apis"
 	apisv1alpha1 "knative.dev/pkg/apis/v1alpha1"
