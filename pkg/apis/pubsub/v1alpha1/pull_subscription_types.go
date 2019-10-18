0cb9b1c129a70f38158c055c65f28c79db780a4a
Ignacio (Nacho) Cano <nachoacano@gmail.com>
2019-10-18 09:54:19 -0700

    fixing types (#346)

--- pkg/apis/pubsub/v1alpha1/pull_subscription_types.go
+++ pkg/apis/pubsub/v1alpha1/pull_subscription_types.go
@@ -168,7 +168,7 @@ func PubSubEventSource(googleCloudProject, topic string) string {
 
 const (
 	// PullSubscription CloudEvent type
-	PubSubPublish = "google.pubsub.topic.publish"
+	PubSubPublish = "com.google.cloud.pubsub.topic.publish"
 )
 
 const (
