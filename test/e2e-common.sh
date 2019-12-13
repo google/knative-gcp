7771571881116425d9393980860b0aa1effdf86d
nachocano <nachocano@google.com>
2019-12-12 18:05:14 -0800

    missing to create namespace

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -79,6 +79,8 @@ function control_plane_setup() {
       --iam-account=${CONTROL_PLANE_SERVICE_ACCOUNT}@${E2E_PROJECT_ID}.iam.gserviceaccount.com
     service_account_key="${CONTROL_PLANE_SERVICE_ACCOUNT_KEY}"
   fi
+  echo "Create the control plane namespace"
+  kubectl create namespace ${CONTROL_PLANE_NAMESPACE}
   echo "Create the control plane secret"
   kubectl -n ${CONTROL_PLANE_NAMESPACE} create secret generic ${CONTROL_PLANE_SECRET_NAME} --from-file=key.json=${service_account_key}
 }
