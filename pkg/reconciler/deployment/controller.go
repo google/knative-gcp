77812dfccec58342043832ba10fc186f3c0d3021
capri-xiyue <52932582+capri-xiyue@users.noreply.github.com>
2020-02-14 10:57:41 -0800

    controller should watch for changes on its secret (#547)
    * controller should watch for changes on its secret
    
    * updated Gopkg.lock
    
    * updated the comments and reorder the deps
    
    * modified code
    
    * updated code-gen

---
+++ pkg/reconciler/deployment/controller.go
@@ -0,0 +1,83 @@
+/*
+Copyright 2020 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package deployment
+
+import (
+	"context"
+
+	"github.com/google/knative-gcp/pkg/apis/duck/v1alpha1"
+	"github.com/google/knative-gcp/pkg/reconciler"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/clock"
+	"k8s.io/client-go/tools/cache"
+	"knative.dev/pkg/client/injection/kube/informers/apps/v1/deployment"
+	"knative.dev/pkg/client/injection/kube/informers/core/v1/secret"
+	"knative.dev/pkg/configmap"
+	"knative.dev/pkg/controller"
+)
+
+const (
+	// ReconcilerName is the name of the reconciler
+	ReconcilerName = "Deployment"
+
+	// controllerAgentName is the string used by this controller to identify
+	// itself when creating events.
+	controllerAgentName = "cloud-run-events-deployment-controller"
+
+	namespace      = "cloud-run-events"
+	secretName     = v1alpha1.DefaultSecretName
+	deploymentName = "controller"
+)
+
+// NewController initializes the controller and is called by the generated code
+// Registers event handlers to enqueue events.
+// When the secret `google-cloud-key` of namespace `cloud-run-events` gets updated, we will enqueue the deployment `controller` of namespace `cloud-run-events`.
+func NewController(
+	ctx context.Context,
+	cmw configmap.Watcher,
+) *controller.Impl {
+
+	deploymentInformer := deployment.Get(ctx)
+	secretInformer := secret.Get(ctx)
+
+	r := &Reconciler{
+		Base:             reconciler.NewBase(ctx, controllerAgentName, cmw),
+		deploymentLister: deploymentInformer.Lister(),
+		clock:           clock.RealClock{},
+	}
+
+	impl := controller.NewImpl(r, r.Logger, ReconcilerName)
+
+	r.Logger.Info("Setting up event handlers")
+
+	sentinel := impl.EnqueueSentinel(types.NamespacedName{Name: deploymentName, Namespace: namespace})
+	secretInformer.Informer().AddEventHandler(cache.FilteringResourceEventHandler{
+		FilterFunc: controller.FilterWithNameAndNamespace(namespace, secretName),
+		Handler:    handleUpdateOnly(sentinel),
+	})
+	return impl
+}
+
+func handleUpdateOnly(h func(interface{})) cache.ResourceEventHandler {
+	return cache.ResourceEventHandlerFuncs{
+		AddFunc:    doNothing,
+		UpdateFunc: controller.PassNew(h),
+		DeleteFunc: doNothing,
+	}
+}
+
+func doNothing(obj interface{}) {
+}
---
+++ pkg/reconciler/deployment/deployment_test.go
@@ -0,0 +1,103 @@
+/*
+Copyright 2019 Google LLC
+
+Licensed under the Apache License, Veroute.on 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package deployment
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/clock"
+	"k8s.io/client-go/kubernetes/scheme"
+	clientgotesting "k8s.io/client-go/testing"
+
+
+	"knative.dev/pkg/configmap"
+	"knative.dev/pkg/controller"
+	logtesting "knative.dev/pkg/logging/testing"
+
+	"github.com/google/knative-gcp/pkg/apis/events/v1alpha1"
+	"github.com/google/knative-gcp/pkg/reconciler"
+
+	. "github.com/google/knative-gcp/pkg/reconciler/testing"
+	. "knative.dev/pkg/reconciler/testing"
+)
+
+const (
+	testDeploymentName = "test-controller"
+	testNS             = "test-cloud-run-events"
+)
+
+func init() {
+	// Add types to scheme
+	_ = v1alpha1.AddToScheme(scheme.Scheme)
+}
+
+func TestAllCases(t *testing.T) {
+
+	table := TableTest{
+		{
+			Name: "bad workqueue key",
+			// Make sure Reconcile handles bad keys.
+			Key: "too/many/parts",
+		}, {
+			Name: "key not found",
+			// Make sure Reconcile handles good keys that don't exist.
+			Key: "foo/not-found",
+		}, {
+			Name: "deployment updated",
+			Objects: []runtime.Object{
+				NewDeployment(testDeploymentName, testNS),
+			},
+			Key: testNS + "/" + testDeploymentName,
+			WantUpdates: []clientgotesting.UpdateActionImpl{{
+				Object: NewDeployment(testDeploymentName, testNS,
+					WithDeploymentAnnotations(map[string]string{
+						"events.cloud.google.com/secretLastObservedUpdateTime": "2019-11-17 20:34:58.651387237 +0000 UTC",
+					}),
+				),
+			}},
+		}, {
+			Name: "deployment with annotation updated",
+			Objects: []runtime.Object{
+				NewDeployment(testDeploymentName, testNS,
+					WithDeploymentAnnotations(map[string]string{
+						"events.cloud.google.com/secretLastObservedUpdateTime": "2019-11-16 20:34:58.651387237 +0000 UTC",
+					})),
+			},
+			Key: testNS + "/" + testDeploymentName,
+			WantUpdates: []clientgotesting.UpdateActionImpl{{
+				Object: NewDeployment(testDeploymentName, testNS,
+					WithDeploymentAnnotations(map[string]string{
+						"events.cloud.google.com/secretLastObservedUpdateTime": "2019-11-17 20:34:58.651387237 +0000 UTC",
+					}),
+				),
+			}},
+		},
+	}
+
+	defer logtesting.ClearAll()
+	table.Test(t, MakeFactory(func(ctx context.Context, listers *Listers, cmw configmap.Watcher, _ map[string]interface{}) controller.Reconciler {
+		return &Reconciler{
+			Base:             reconciler.NewBase(ctx, controllerAgentName, cmw),
+			deploymentLister: listers.GetDeploymentLister(),
+			clock: clock.NewFakeClock(time.Date(
+				2019, 11, 17, 20, 34, 58, 651387237, time.UTC)),
+		}
+	}))
+}
