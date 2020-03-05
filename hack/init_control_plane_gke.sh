25807169e5aa298b1b3163363b6b3a00008f7dac
grac3gao <gracegao@google.com>
2020-03-04 22:11:24 -0800

    control-plane-2

--- hack/init_control_plane_gke.sh
+++ hack/init_control_plane_gke.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Usage: ./init_control_plane.sh
+# Usage: ./init_control_plane_gke.sh
 # The current project set in gcloud MUST be the same as where the cluster is running.
 
 NAMESPACE=cloud-run-events
@@ -31,11 +31,11 @@ gcloud services enable cloudscheduler.googleapis.com
 gcloud services enable logging.googleapis.com
 gcloud services enable stackdriver.googleapis.com
 
-# Enable workload identity
+# Enable workload identity.
 gcloud beta container clusters update ${CLUSTER_NAME} \
   --identity-namespace=${PROJECT_ID}.svc.id.goog
 
-# Create the service account for the control plane
+# Create the service account for the control plane.
 gcloud iam service-accounts create ${GSA_NAME}
 
 # Grant permissions to the service account for the control plane to manage native GCP resources.
@@ -46,9 +46,9 @@ gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${G
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${GSA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com --role roles/logging.privateLogViewer
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${GSA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com --role roles/iam.serviceAccountAdmin
 
-# Allow the Kubernetes service account to use the Google service account
+# Allow the Kubernetes service account to use Google service account.
 MEMBER="serviceAccount:"${PROJECT_ID}".svc.id.goog["${NAMESPACE}"/"${KSA_NAME}"]"
 gcloud iam service-accounts add-iam-policy-binding --role roles/iam.workloadIdentityUser --member $MEMBER ${GSA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com
 
-# Add annotation to KSA
+# Add annotation to Kubernetes service account.
 kubectl annotate serviceaccount --namespace ${NAMESPACE} ${KSA_NAME} iam.gke.io/gcp-service-account=${GSA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com
