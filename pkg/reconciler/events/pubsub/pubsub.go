fcf25924dbd9be39d0fc8cfabe2096d011febc29
Nicolas Lopez <ngiraldo@google.com>
2020-03-09 12:51:06 -0400

    more review comments

--- pkg/reconciler/events/pubsub/pubsub.go
+++ pkg/reconciler/events/pubsub/pubsub.go
@@ -37,11 +37,13 @@ import (
 )
 
 const (
-	creatingFailedReason    = "PullSubscriptionGetFailed"
-	finalizerName           = controllerAgentName
+	finalizerName = controllerAgentName
+	resourceGroup = "cloudpubsubsources.events.cloud.google.com"
+
+	createFailedReason      = "PullSubscriptionCreateFailed"
+	getFailedReason         = "PullSubscriptionGetFailed"
 	reconciledSuccessReason = "CloudPubSubSourceReconciled"
 	reconciledFailedReason  = "PullSubscriptionReconcileFailed"
-	resourceGroup           = "cloudpubsubsources.events.cloud.google.com"
 )
 
 // Reconciler is the controller implementation for the CloudPubSubSource source.
@@ -63,6 +65,7 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, pubsub *v1alpha1.CloudPu
 	ctx = logging.WithLogger(ctx, r.Logger.With(zap.Any("pubsub", pubsub)))
 
 	pubsub.Status.InitializeConditions()
+	pubsub.Status.ObservedGeneration = pubsub.Generation
 
 	if pubsub.DeletionTimestamp != nil {
 		// No finalizer needed, the pullsubscription will be garbage collected.
@@ -74,8 +77,6 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, pubsub *v1alpha1.CloudPu
 		pubsub.Status.MarkPullSubscriptionFailed(reconciledFailedReason, "Failed to reconcile PullSubscription: %s", event.Error())
 		return event
 	}
-	// Set the ObservedGeneration after successful reconcile
-	pubsub.Status.ObservedGeneration = pubsub.Generation
 	pubsub.Status.PropagatePullSubscriptionStatus(&ps.Status)
 
 	// Sink has been resolved from the underlying PullSubscription, set it here.
@@ -86,7 +87,7 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, pubsub *v1alpha1.CloudPu
 	} else {
 		pubsub.Status.SinkURI = sinkURI
 	}
-	return pkgreconciler.NewEvent(corev1.EventTypeNormal, reconciledSuccessReason, `PullSubscription reconciled: "%s/%s"`, pubsub.Namespace, pubsub.Name)
+	return pkgreconciler.NewEvent(corev1.EventTypeNormal, reconciledSuccessReason, `CloudPubSubSource reconciled: "%s/%s"`, pubsub.Namespace, pubsub.Name)
 }
 
 func (r *Reconciler) reconcilePullSubscription(ctx context.Context, source *v1alpha1.CloudPubSubSource) (*pubsubv1alpha1.PullSubscription, pkgreconciler.Event) {
@@ -94,7 +95,7 @@ func (r *Reconciler) reconcilePullSubscription(ctx context.Context, source *v1al
 	if err != nil {
 		if !apierrs.IsNotFound(err) {
 			logging.FromContext(ctx).Desugar().Error("Failed to get PullSubscription", zap.Error(err))
-			return nil, pkgreconciler.NewEvent(corev1.EventTypeWarning, creatingFailedReason, "Getting PullSubscription failed with: %s", err)
+			return nil, pkgreconciler.NewEvent(corev1.EventTypeWarning, getFailedReason, "Getting PullSubscription failed with: %s", err)
 		}
 		args := &resources.PullSubscriptionArgs{
 			Namespace:   source.Namespace,
@@ -111,7 +112,7 @@ func (r *Reconciler) reconcilePullSubscription(ctx context.Context, source *v1al
 		ps, err = r.RunClientSet.PubsubV1alpha1().PullSubscriptions(newPS.Namespace).Create(newPS)
 		if err != nil {
 			logging.FromContext(ctx).Desugar().Error("Failed to create PullSubscription", zap.Error(err))
-			return nil, pkgreconciler.NewEvent(corev1.EventTypeWarning, creatingFailedReason, "Creating PullSubscription failed with: %s", err)
+			return nil, pkgreconciler.NewEvent(corev1.EventTypeWarning, createFailedReason, "Creating PullSubscription failed with: %s", err)
 		}
 	}
 	return ps, nil
