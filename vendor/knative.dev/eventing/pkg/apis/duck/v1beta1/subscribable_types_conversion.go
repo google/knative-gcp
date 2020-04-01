8753510c25a11989a6259618c2e544d3416c79ad
Ian Milligan <ianmllgn@gmail.com>
2020-04-01 12:48:13 -0700

    Remove SetSpanID patch and update deps

--- vendor/knative.dev/eventing/pkg/apis/duck/v1beta1/subscribable_types_conversion.go
+++ vendor/knative.dev/eventing/pkg/apis/duck/v1beta1/subscribable_types_conversion.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 The Knative Authors
+Copyright 2020 The Knative Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,23 +14,21 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package v1alpha2
+package v1beta1
 
 import (
 	"context"
+	"fmt"
+
+	"knative.dev/pkg/apis"
 )
 
-func (s *ApiServerSource) SetDefaults(ctx context.Context) {
-	s.Spec.SetDefaults(ctx)
+// ConvertTo implements apis.Convertible
+func (source *Subscribable) ConvertTo(ctx context.Context, sink apis.Convertible) error {
+	return fmt.Errorf("v1beta1 is the highest known version, got: %T", sink)
 }
 
-func (ss *ApiServerSourceSpec) SetDefaults(ctx context.Context) {
-
-	if ss.EventMode == "" {
-		ss.EventMode = ReferenceMode
-	}
-
-	if ss.ServiceAccountName == "" {
-		ss.ServiceAccountName = "default"
-	}
+// ConvertFrom implements apis.Convertible
+func (sink *Subscribable) ConvertFrom(ctx context.Context, source apis.Convertible) error {
+	return fmt.Errorf("v1beta1 is the highest known version, got: %T", source)
 }
