2e1871e0b2de4772fddb4556fb2d749906f009e4
cshou <cshou@google.com>
2020-04-08 11:47:02 -0700

    Fix flaky volume config test with atomic file write (#780)
    * Fix flakcy volume config test with atomic file write
    
    * use a different folder
    
    * Update pkg/broker/config/volume/volume_test.go
    
    Co-Authored-By: Adam Harwayne <harwayne@google.com>
    
    * fix
    
    Co-authored-by: Adam Harwayne <harwayne@google.com>

--- pkg/broker/config/volume/volume_test.go
+++ pkg/broker/config/volume/volume_test.go
@@ -108,14 +108,18 @@ func TestSyncConfigFromFile(t *testing.T) {
 	}
 	tmp, err := ioutil.TempFile(dir, "test-*")
 	if err != nil {
-		t.Fatalf("unexpected error from creating temp file: %v", err)
+		t.Fatalf("unexpected error from creating config file: %v", err)
 	}
 	defer func() {
 		tmp.Close()
 		os.RemoveAll(dir)
 	}()
-	tmp.Write(b)
-	tmp.Close()
+	if _, err := tmp.Write(b); err != nil {
+		t.Fatalf("unexpected error from writing config file: %v", err)
+	}
+	if err := tmp.Close(); err != nil {
+		t.Fatalf("unexpected error from closing config file: %v", err)
+	}
 
 	ch := make(chan struct{}, 1)
 
@@ -154,12 +158,36 @@ func TestSyncConfigFromFile(t *testing.T) {
 
 	delete(data.Brokers["ns2/broker2"].Targets, "name4")
 	b, _ = proto.Marshal(data)
-	ioutil.WriteFile(tmp.Name(), b, 0644)
+	atomicWriteFile(t, tmp.Name(), b)
 
 	<-ch
 
 	gotTargets = targets.(*Targets).Load()
 	if !proto.Equal(data, gotTargets) {
-		t.Errorf("updated targets got=%+v, want%+v", gotTargets, data)
+		t.Errorf("updated targets got=%+v, want=%+v", gotTargets, data)
+	}
+}
+
+func atomicWriteFile(t *testing.T, file string, bytes []byte) {
+	t.Helper()
+	// In order to more closely replicate how K8s writes ConfigMaps to the file system, we will
+	// atomically swap out the file by writing it to a temp directory, then renaming it into the
+	// directory we are watching.
+	dir, err := ioutil.TempDir("", "temp-*")
+	if err != nil {
+		t.Fatalf("unexpected error from creating temp dir: %v", err)
+	}
+	defer os.RemoveAll(dir)
+	tmpFile, err := ioutil.TempFile(dir, "temp-*")
+	if err != nil {
+		t.Fatalf("unexpected error from creating temp file: %v", err)
+	}
+	defer os.Remove(tmpFile.Name())
+
+	if _, err = tmpFile.Write(bytes); err != nil {
+		t.Fatalf("unexpected error from writing temp file: %v", err)
+	}
+	if err := os.Rename(tmpFile.Name(), file); err != nil {
+		t.Fatalf("unexpected error from renaming temp file: %v", err)
 	}
 }
