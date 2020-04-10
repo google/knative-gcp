774b62a256ffdbbdc7e06d6db3e31b1df20f2e30
grac3gao <gracegao@google.com>
2020-04-09 18:27:34 -0700

    Revert "update configure"
    This reverts commit fa4c9bb3

--- pkg/reconciler/identity/reconciler.go
+++ pkg/reconciler/identity/reconciler.go
@@ -53,12 +53,12 @@ const (
 // defaultRetry represents that there will be 3 iterations.
 // The duration starts from 100ms and is multiplied by factor 2.0 for each iteration.
 var defaultRetry = wait.Backoff{
-	Steps:    5,
+	Steps:    3,
 	Duration: 200 * time.Millisecond,
 	Factor:   2.0,
 	// The sleep at each iteration is the duration plus an additional
 	// amount chosen uniformly at random from the interval between 0 and jitter*duration.
-	Jitter: 1.0,
+	Jitter: 0.5,
 }
 
 func NewIdentity(ctx context.Context) *Identity {
