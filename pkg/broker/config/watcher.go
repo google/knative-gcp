bbd6dca4a040dda2e8c9264701aa27cdc18fe574
Ian Milligan <ianmllgn@gmail.com>
2020-04-11 21:41:50 -0700

    Add TargetsWatcher for broadcasting TargetsConfig updates
    TargetsWatcher uses a channel to indicate the available of an updated
    TargetsConfig which is returned alongside each config read.

---
+++ pkg/broker/config/watcher.go
@@ -0,0 +1,49 @@
+package config
+
+// TargetsWatcher is an interface for watching a dynamically updated TargetsConfig.
+type TargetsWatcher interface {
+	// Returns the latest TargetsConfig value in
+	// Targets.Config. Targets.Updated contains a channel which will be
+	// closed when there is new config value to be read.
+	Targets() Targets
+}
+
+// Targets contains a single Config as well as an Updated channel which will be
+// closed after Config becomes stale.
+type Targets struct {
+	Config  *TargetsConfig
+	Updated chan struct{}
+}
+
+type watcher <-chan Targets
+
+func (w watcher) Targets() Targets {
+	return <-w
+}
+
+// NewTargetsWatcher creates a new TargetsWatcher which watches for updates on
+// in until in is closed
+func NewTargetsWatcher(in <-chan *TargetsConfig) TargetsWatcher {
+	w := make(chan Targets)
+	go watch(in, w)
+	return watcher(w)
+}
+
+func watch(in <-chan *TargetsConfig, out chan<- Targets) {
+	defer close(out)
+	targets := Targets{Updated: make(chan struct{})}
+	for {
+		select {
+		case out <- targets:
+		case c, ok := <-in:
+			if !ok {
+				return
+			}
+			close(targets.Updated)
+			targets = Targets{
+				Config:  c,
+				Updated: make(chan struct{}),
+			}
+		}
+	}
+}
---
+++ pkg/broker/config/watcher_test.go
@@ -0,0 +1,80 @@
+package config_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/knative-gcp/pkg/broker/config"
+)
+
+func TestWatcherNoConfig(t *testing.T) {
+	t.Parallel()
+	in := make(chan *config.TargetsConfig)
+	defer close(in)
+	watcher := config.NewTargetsWatcher(in)
+	targets := watcher.Targets()
+	if targets.Config != nil {
+		t.Errorf("expected targets.Config to be nil, got: %v", targets.Config)
+	}
+}
+
+func TestWatcherWithConfig(t *testing.T) {
+	t.Parallel()
+	in := make(chan *config.TargetsConfig)
+	defer close(in)
+	watcher := config.NewTargetsWatcher(in)
+	want := new(config.TargetsConfig)
+	in <- want
+	targets := watcher.Targets()
+	if targets.Config != want {
+		t.Errorf("targets.Config: want %v, got %v", want, targets.Config)
+	}
+}
+
+func TestWatcherNotUpdated(t *testing.T) {
+	t.Parallel()
+	in := make(chan *config.TargetsConfig)
+	defer close(in)
+	watcher := config.NewTargetsWatcher(in)
+	targets := watcher.Targets()
+	select {
+	case <-targets.Updated:
+		t.Error("targets.Updated signalled spuriously")
+	case <-time.After(1 * time.Millisecond):
+	}
+}
+
+func TestWatcherUpdated(t *testing.T) {
+	t.Parallel()
+	in := make(chan *config.TargetsConfig)
+	defer close(in)
+	watcher := config.NewTargetsWatcher(in)
+	targets := watcher.Targets()
+	in <- new(config.TargetsConfig)
+	select {
+	case <-targets.Updated:
+	case <-time.After(1 * time.Millisecond):
+		t.Error("targets.Updated not signalled")
+	}
+}
+
+func TestWatcherFanout(t *testing.T) {
+	t.Parallel()
+	in := make(chan *config.TargetsConfig)
+	defer close(in)
+	watcher := config.NewTargetsWatcher(in)
+	targets1 := watcher.Targets()
+	targets2 := watcher.Targets()
+	want := new(config.TargetsConfig)
+	in <- want
+	<-targets1.Updated
+	targets1 = watcher.Targets()
+	<-targets2.Updated
+	targets2 = watcher.Targets()
+	if targets1.Config != want {
+		t.Errorf("targets1.Config: want %v, got %v", want, targets1.Config)
+	}
+	if targets2.Config != want {
+		t.Errorf("targets2.Config: want %v, got %v", want, targets2.Config)
+	}
+}
