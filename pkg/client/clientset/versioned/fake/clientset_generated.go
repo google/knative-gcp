9acfc4f6a74dab8280470ecde86fed9cd1573b4a
Grant Rodgers <grantrodgers@google.com>
2020-04-23 13:12:13 -0700

    Move BrokerCell to v1alpha1 version

--- pkg/client/clientset/versioned/fake/clientset_generated.go
+++ pkg/client/clientset/versioned/fake/clientset_generated.go
@@ -26,8 +26,8 @@ import (
 	fakeeventsv1alpha1 "github.com/google/knative-gcp/pkg/client/clientset/versioned/typed/events/v1alpha1/fake"
 	eventsv1beta1 "github.com/google/knative-gcp/pkg/client/clientset/versioned/typed/events/v1beta1"
 	fakeeventsv1beta1 "github.com/google/knative-gcp/pkg/client/clientset/versioned/typed/events/v1beta1/fake"
-	internalv1beta1 "github.com/google/knative-gcp/pkg/client/clientset/versioned/typed/intevents/v1beta1"
-	fakeinternalv1beta1 "github.com/google/knative-gcp/pkg/client/clientset/versioned/typed/intevents/v1beta1/fake"
+	internalv1alpha1 "github.com/google/knative-gcp/pkg/client/clientset/versioned/typed/intevents/v1alpha1"
+	fakeinternalv1alpha1 "github.com/google/knative-gcp/pkg/client/clientset/versioned/typed/intevents/v1alpha1/fake"
 	messagingv1alpha1 "github.com/google/knative-gcp/pkg/client/clientset/versioned/typed/messaging/v1alpha1"
 	fakemessagingv1alpha1 "github.com/google/knative-gcp/pkg/client/clientset/versioned/typed/messaging/v1alpha1/fake"
 	messagingv1beta1 "github.com/google/knative-gcp/pkg/client/clientset/versioned/typed/messaging/v1beta1"
@@ -107,9 +107,9 @@ func (c *Clientset) EventsV1beta1() eventsv1beta1.EventsV1beta1Interface {
 	return &fakeeventsv1beta1.FakeEventsV1beta1{Fake: &c.Fake}
 }
 
-// InternalV1beta1 retrieves the InternalV1beta1Client
-func (c *Clientset) InternalV1beta1() internalv1beta1.InternalV1beta1Interface {
-	return &fakeinternalv1beta1.FakeInternalV1beta1{Fake: &c.Fake}
+// InternalV1alpha1 retrieves the InternalV1alpha1Client
+func (c *Clientset) InternalV1alpha1() internalv1alpha1.InternalV1alpha1Interface {
+	return &fakeinternalv1alpha1.FakeInternalV1alpha1{Fake: &c.Fake}
 }
 
 // MessagingV1alpha1 retrieves the MessagingV1alpha1Client
--- pkg/client/informers/externalversions/generic.go
+++ pkg/client/informers/externalversions/generic.go
@@ -24,7 +24,7 @@ import (
 	v1beta1 "github.com/google/knative-gcp/pkg/apis/broker/v1beta1"
 	v1alpha1 "github.com/google/knative-gcp/pkg/apis/events/v1alpha1"
 	eventsv1beta1 "github.com/google/knative-gcp/pkg/apis/events/v1beta1"
-	inteventsv1beta1 "github.com/google/knative-gcp/pkg/apis/intevents/v1beta1"
+	inteventsv1alpha1 "github.com/google/knative-gcp/pkg/apis/intevents/v1alpha1"
 	messagingv1alpha1 "github.com/google/knative-gcp/pkg/apis/messaging/v1alpha1"
 	messagingv1beta1 "github.com/google/knative-gcp/pkg/apis/messaging/v1beta1"
 	policyv1alpha1 "github.com/google/knative-gcp/pkg/apis/policy/v1alpha1"
@@ -88,9 +88,9 @@ func (f *sharedInformerFactory) ForResource(resource schema.GroupVersionResource
 	case eventsv1beta1.SchemeGroupVersion.WithResource("cloudstoragesources"):
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Events().V1beta1().CloudStorageSources().Informer()}, nil
 
-		// Group=internal.events.cloud.google.com, Version=v1beta1
-	case inteventsv1beta1.SchemeGroupVersion.WithResource("brokercells"):
-		return &genericInformer{resource: resource.GroupResource(), informer: f.Internal().V1beta1().BrokerCells().Informer()}, nil
+		// Group=internal.events.cloud.google.com, Version=v1alpha1
+	case inteventsv1alpha1.SchemeGroupVersion.WithResource("brokercells"):
+		return &genericInformer{resource: resource.GroupResource(), informer: f.Internal().V1alpha1().BrokerCells().Informer()}, nil
 
 		// Group=messaging.cloud.google.com, Version=v1alpha1
 	case messagingv1alpha1.SchemeGroupVersion.WithResource("channels"):
