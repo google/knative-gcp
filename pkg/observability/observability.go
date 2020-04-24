913d065b465a085b17b75510cf8af4a41aeffffd
Ian Milligan <ianmllgn@gmail.com>
2020-04-24 13:26:38 -0700

    Use sharedmain setup functions

--- pkg/observability/observability.go
+++ pkg/observability/observability.go
@@ -3,17 +3,13 @@ package observability
 
 import (
 	"context"
-	"log"
 	"os"
 
 	"go.uber.org/zap"
 	"knative.dev/eventing/pkg/tracing"
-	kubeclient "knative.dev/pkg/client/injection/kube/client"
-	"knative.dev/pkg/configmap"
 	"knative.dev/pkg/injection/sharedmain"
 	"knative.dev/pkg/logging"
 	"knative.dev/pkg/metrics"
-	"knative.dev/pkg/system"
 	tracingconfig "knative.dev/pkg/tracing/config"
 )
 
@@ -21,17 +17,13 @@ import (
 // configmaps. Returns an updated context with logging and function to flush telemetry which should
 // be called before exit.
 func SetupDynamicConfig(ctx context.Context, componentName string) (context.Context, func(), error) {
+	sharedmain.MemStatsOrDie(ctx)
 	// Set up our logger.
-	loggingConfig, err := sharedmain.GetLoggingConfig(ctx)
-	if err != nil {
-		log.Fatal("Error loading/parsing logging configuration: ", err)
-	}
-	logger, atomicLevel := logging.NewLoggerFromConfig(loggingConfig, componentName)
+	logger, atomicLevel := sharedmain.SetupLoggerOrDie(ctx, componentName)
 	// Watch the logging config map and dynamically update logging levels.
-	configMapWatcher := configmap.NewInformedWatcher(kubeclient.Get(ctx), system.Namespace())
-
+	configMapWatcher := sharedmain.SetupConfigMapWatchOrDie(ctx, logger)
 	// Watch the logging config map and dynamically update logging levels.
-	configMapWatcher.Watch(logging.ConfigMapName(), logging.UpdateLevelFromConfigMap(logger, atomicLevel, componentName))
+	sharedmain.WatchLoggingConfigOrDie(ctx, configMapWatcher, logger, atomicLevel, componentName)
 	// Watch the observability config map
 	configMapWatcher.Watch(metrics.ConfigMapName(), metrics.UpdateExporterFromConfigMap(componentName, logger))
 	// Watch the tracing config map
@@ -41,7 +33,7 @@ func SetupDynamicConfig(ctx context.Context, componentName string) (context.Cont
 
 	configmapCtx, cancel := context.WithCancel(ctx)
 	// configMapWatcher does not block, so start it first.
-	if err = configMapWatcher.Start(configmapCtx.Done()); err != nil {
+	if err := configMapWatcher.Start(configmapCtx.Done()); err != nil {
 		logger.Warn("Failed to start ConfigMap watcher", zap.Error(err))
 	}
 	return logging.WithLogger(ctx, logger), func() { cancel(); flushExporters(logger) }, nil
