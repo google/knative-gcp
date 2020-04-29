5cba0e9d81414d1522b4ee3f982d0da9db9e0713
Ian Milligan <ianmllgn@gmail.com>
2020-04-28 23:46:15 -0700

    Update licenses

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -54,3 +54,6 @@ go mod tidy
 go mod vendor
 
 find vendor/ \( -name OWNERS -o -name OWNERS_ALIASES -o -name BUILD -o -name BUILD.bazel \) -delete
+
+update_licenses third_party/VENDOR-LICENSE \
+  $(grep -rl --include '*.go' --exclude-dir vendor "package main" . | xargs -n1 dirname | uniq)
