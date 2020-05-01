6c4ec4c5b656c66ef4d2023eeed5d5065c265352
Ian Milligan <ianmllgn@gmail.com>
2020-05-01 14:51:24 -0700

    Enable GO111MODULE mode in verify-codegen

--- hack/verify-codegen.sh
+++ hack/verify-codegen.sh
@@ -18,6 +18,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+export GO111MODULE=on
+
 source $(dirname $0)/../vendor/knative.dev/test-infra/scripts/library.sh
 
 readonly TMP_DIFFROOT="$(mktemp -d ${REPO_ROOT_DIR}/tmpdiffroot.XXXXXX)"
