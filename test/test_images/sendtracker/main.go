235990c62893a16e4201e1263475def2b0ce7da8
Eric Lemar <elemar@google.com>
2020-05-04 22:34:01 -0700

    New test image for testing that messages that are accepted by the receiver are actually delivered.

---
+++ test/test_images/sendtracker/main.go
@@ -0,0 +1,140 @@
+/*
+Copyright 2020 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	"math"
+	"math/big"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"knative.dev/eventing/pkg/kncloudevents"
+)
+
+// Compare the ranges in sendResults and receiveResults.
+// Create error strings for any entries that are successful in sendResults, but not receiveResults.
+// Create warning strings for any entries that are in receiveResults, but not successfull in sendResults.
+// Append an error string if sendError is not nil.
+func compareLogs(sendResults rangeResultArr, receiveResults rangeReceivedArr, sendError error) (errorLogs, warningLogs string) {
+	successSendRangeCount := 0
+	if sendError != nil {
+		errorLogs = sendError.Error()
+	}
+	for _, senderI := range sendResults {
+		if senderI.success {
+			successSendRangeCount++
+			found := false
+			for _, receiverI := range receiveResults {
+				if receiverI.minID <= senderI.minID && receiverI.maxID >= senderI.maxID {
+					found = true
+					if receiverI.minID != senderI.minID || receiverI.maxID != senderI.maxID {
+						warningLogs = fmt.Sprintf("%sExtra elements in received range for sender range %+v\n", warningLogs, senderI)
+					}
+				}
+			}
+			if !found {
+				errorLogs = fmt.Sprintf("%sElements not received from %+v\n", errorLogs, senderI)
+			}
+
+		}
+	}
+	if successSendRangeCount == 0 {
+		errorLogs = fmt.Sprintf("%sNo successful sends\n", errorLogs)
+	}
+	if successSendRangeCount < len(receiveResults) {
+		warningLogs = fmt.Sprintf("%sExtra range in received range set\n", warningLogs)
+	}
+	return errorLogs, warningLogs
+}
+
+func main() {
+	sinkURI := os.Getenv("K_SINK")
+	if len(sinkURI) == 0 {
+		fmt.Printf("No SINK URI specified\n")
+		os.Exit(1)
+	}
+	delayMSString := os.Getenv("DELAY_MS")
+	delayMS, err := strconv.Atoi(delayMSString)
+	if err != nil || delayMS < 0 {
+		fmt.Printf("Invalid delay millisecond (%s)", delayMSString)
+		os.Exit(1)
+	}
+	postStopWaitSecsString := os.Getenv("POST_STOP_SECS")
+	postStopWaitSecs, err := strconv.Atoi(postStopWaitSecsString)
+	if err != nil || postStopWaitSecs < 1 {
+		fmt.Printf("Invalid post stop wait seconds (%s)", postStopWaitSecsString)
+		os.Exit(1)
+	}
+	fmt.Printf("Opening connection to %s\n", sinkURI)
+	ceClient, err := kncloudevents.NewDefaultClient(sinkURI)
+	if err != nil {
+		fmt.Printf("Unable to create ceClient: %s ", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Client created to %s\n", sinkURI)
+
+	bint, err := rand.Int(rand.Reader, big.NewInt(math.MaxInt64))
+	if err != nil {
+		fmt.Printf("Error creating stracker random source id: %v", bint)
+		os.Exit(1)
+	}
+	source := fmt.Sprintf("stracker_%s", bint)
+	// Start the receive goroutine
+	receiver := StartReceiver(source, time.Duration(postStopWaitSecs)*time.Second)
+	sendPacer := makeSender(ceClient, receiver, source, time.Duration(delayMS))
+
+	http.HandleFunc("/stop", sendPacer.handleStop)
+	http.HandleFunc("/start", sendPacer.handleStart)
+	http.HandleFunc("/ready", sendPacer.handleReady)
+	http.HandleFunc("/resultsready", sendPacer.handleResultsReady)
+	http.HandleFunc("/results", sendPacer.handleResults)
+	go http.ListenAndServe(":8070", nil)
+
+	// Run the send path synchronously
+	sendError := sendPacer.Run()
+	if sendError != nil && sendPacer.getState() < SendStateFinishing {
+		fmt.Printf("Sender saw an error and never made it to finishing: %v\n", sendError)
+	} else {
+		fmt.Printf("Waiting for receiver to drain\n")
+		<-receiver.fullyDone
+		// Get the events the receiver saw
+		results, dups := receiver.GetResults()
+
+		// Compare with the events the sender sent
+		errorString, warningString := compareLogs(sendPacer.results, results, sendError)
+
+		var resultSummary string
+		// If there were no errors seen, it was a success (warnings don't fail this)
+		if len(errorString) == 0 {
+			resultSummary = "success\n"
+		} else {
+			resultSummary = "failure\n"
+		}
+		sendPacer.resultSummary = fmt.Sprintf("%s%s%sAll send intervals = %s\nAll receive intervals = %s\nDuplicates seen %d\n", resultSummary, errorString, warningString, sendPacer.results, results, dups)
+		fmt.Printf("%s", sendPacer.resultSummary)
+		// Allow the result to be retrieved via /results
+		sendPacer.setState(SendStateDone)
+	}
+
+	// loop forever
+	for {
+		time.Sleep(time.Minute)
+	}
+}
---
+++ test/test_images/sendtracker/receivetracker.go
@@ -0,0 +1,220 @@
+/*
+Copyright 2020 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sort"
+	"strconv"
+	"sync"
+	"time"
+
+	cloudevents "github.com/cloudevents/sdk-go"
+)
+
+const (
+	// Waiting for the sender to send a connect message
+	ReceiveStateConnecting = iota
+	// Receiving messages
+	ReceiveStateReady
+	// Receiving messages, but will stop after timeout
+	ReceiveStateFinishing
+	// Will not record any more messages
+	ReceiveStateDone
+)
+
+type Receiver struct {
+	startTime time.Time
+	client    cloudevents.Client
+	// Hold received message id's as we get them.  For
+	// now only compress the results after taking data.
+	// Revisit if memory is an issue.
+	resultMap map[int]struct{}
+	// Count of duplicates we see whan adding to map
+	resultDuplicates int
+	// sumarized results once we are in Done
+	resultList rangeReceivedArr
+	// Lock for modifying resultMap
+	resultLock sync.Mutex
+	// Lock for modifying state
+	stateLock sync.Mutex
+	// state of the receiver
+	state int
+	// channel closed when we have hit the Done state.
+	fullyDone chan struct{}
+	// name of the source to accept events from
+	receiveSource string
+	// Time to wait between receiving the done message and stopping to listen
+	// for events
+	waitAfterDone time.Duration
+}
+
+func (r *Receiver) GetState() int {
+	r.stateLock.Lock()
+	defer r.stateLock.Unlock()
+	return r.state
+}
+
+// Transitions occur due to event deliveries that can be duplicated, so fail to change
+// but don't error on invalid transitions.
+func (r *Receiver) setState(newState int) bool {
+	r.stateLock.Lock()
+	defer r.stateLock.Unlock()
+	// Transition either in the normal order, or if the done call never succeeded and we are forced
+	// to done.
+	if r.state == newState-1 || newState == ReceiveStateDone && r.state == ReceiveStateReady {
+		r.state = newState
+		return true
+	}
+	return false
+}
+
+func (r *Receiver) GetResults() (rangeReceivedArr, int) {
+	r.resultLock.Lock()
+	defer r.resultLock.Unlock()
+	state := r.GetState()
+	if state != ReceiveStateDone {
+		log.Fatalf("Bad state for get results: %d", state)
+	}
+	return r.resultList, r.resultDuplicates
+}
+
+func StartReceiver(receiveSource string, waitAfterDone time.Duration) *Receiver {
+	client, err := cloudevents.NewDefaultClient()
+	if err != nil {
+		panic(err)
+	}
+	r := &Receiver{
+		client:        client,
+		startTime:     time.Now(),
+		resultMap:     make(map[int]struct{}),
+		fullyDone:     make(chan struct{}),
+		receiveSource: receiveSource,
+		waitAfterDone: waitAfterDone,
+	}
+
+	fmt.Printf("\nReceiver waiting to start\n")
+	go func() {
+		if err := r.client.StartReceiver(context.Background(), r.Receive); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	return r
+}
+
+type rangeReceived struct {
+	minID int
+	maxID int
+}
+
+type rangeReceivedArr []rangeReceived
+
+func (r rangeReceivedArr) String() string {
+	result := fmt.Sprintf("rangeReceived{\n")
+	if len(r) == 0 {
+		result += "  nil\n"
+	}
+	for i := range r {
+		result += fmt.Sprintf("  range: %d-%d\n", r[i].minID, r[i].maxID)
+	}
+	result = result + "}\n"
+	return result
+}
+
+// Convert a map of ints into an ordered list of maximal length range
+// extents for all elements in the map.
+func mapToRange(mapIn map[int]struct{}) rangeReceivedArr {
+	var listOut rangeReceivedArr
+	allKey := make([]int, 0, len(mapIn))
+	for id, _ := range mapIn {
+		allKey = append(allKey, id)
+	}
+	sort.Ints(allKey)
+
+	for _, key := range allKey {
+		if len(listOut) == 0 || listOut[len(listOut)-1].maxID+1 != key {
+			listOut = append(listOut, rangeReceived{minID: key, maxID: key})
+		} else {
+			listOut[len(listOut)-1].maxID = key
+		}
+	}
+	return listOut
+}
+
+// Called when we are fully done receiving events.  This either happens
+// after a delay after we received the finished event, or is called directly
+// by the sender if it never successfully gets a finish event to us.
+func (r *Receiver) Done() {
+	r.resultLock.Lock()
+	changed := r.setState(ReceiveStateDone)
+
+	// Only compute the list on the first Done
+	if !changed {
+		r.resultLock.Unlock()
+		return
+	}
+	r.resultList = mapToRange(r.resultMap)
+	r.resultLock.Unlock()
+	close(r.fullyDone)
+}
+
+func (r *Receiver) Receive(ctx context.Context, event cloudevents.Event, resp *cloudevents.EventResponse) {
+	if event.Source() == r.receiveSource {
+		switch event.Type() {
+		case "event-trace-connect":
+			r.setState(ReceiveStateReady)
+			return
+		case "event-trace-event":
+			id, err := strconv.Atoi(event.ID())
+			if err != nil {
+				fmt.Errorf("Bad ID Decoding: %v %s", err, event)
+			}
+
+			if id <= 0 {
+				fmt.Errorf("Invalid ID %d: %s", id, event)
+			}
+
+			r.resultLock.Lock()
+			state := r.GetState()
+			if state == ReceiveStateReady || state == ReceiveStateFinishing {
+				_, exists := r.resultMap[id]
+				if !exists {
+					r.resultMap[id] = struct{}{}
+				} else {
+					r.resultDuplicates++
+				}
+			}
+			r.resultLock.Unlock()
+
+		case "event-trace-done":
+			change := r.setState(ReceiveStateFinishing)
+			// Only enqueue a wakeup to transition to Done once
+			if change {
+				fmt.Printf("Saw done request\n")
+				// Wait to allow delayed event delivery to come in.
+				time.AfterFunc(r.waitAfterDone, r.Done)
+			}
+			return
+		default:
+			fmt.Errorf("Unexpected event type %s: %s", event.Type(), event)
+			return
+		}
+	}
+
+}
