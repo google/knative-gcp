2858c74a9f5e8511f860ffb3709a683c9f94806b
Chi Zhang <chizhg@google.com>
2020-05-07 21:31:16 -0700

    fix

--- test/upload-test-images.sh
+++ test/upload-test-images.sh
@@ -22,12 +22,12 @@ function upload_test_images() {
   local docker_tag=$2
   local tag_option=""
   if [ -n "${docker_tag}" ]; then
-    tag_option="--tags $docker_tag,latest"
+    tag_option="$docker_tag,latest"
   fi
 
   # ko resolve is being used for the side-effect of publishing images,
   # so the resulting yaml produced is ignored.
-  ko resolve --strict "${tag_option}" -RBf "${image_dir}" > /dev/null
+  ko resolve --strict --tags "${tag_option}" -RBf "${image_dir}" > /dev/null
 }
 
 : "${KO_DOCKER_REPO:?"You must set 'KO_DOCKER_REPO', see DEVELOPMENT.md"}"
