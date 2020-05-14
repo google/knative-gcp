7c7cdf0bf6d41fe9f70e6ea3ccf936cebe7cf5eb
XiyueYu <xiyue@google.com>
2020-05-14 16:50:24 -0700

    moved controller pods dumping to one specific repo

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -103,14 +103,15 @@ function dump_extra_cluster_state() {
   # Collecting logs from all namespace `cloud-run-events` pods.
   echo "============================================================"
   local namespace=${CONTROL_PLANE_NAMESPACE}
+  local controller_logs_repo="controller-logs"
   for pod in $(kubectl get pod -n $namespace | grep Running | awk '{print $1}' ); do
     for container in $(kubectl get pod "${pod}" -n $namespace -ojsonpath='{.spec.containers[*].name}'); do
-      local current_output="${ARTIFACTS}/k8s.dump-$(basename ${E2E_SCRIPT})-${namespace}-${pod}-${container}.txt"
+      local current_output="${ARTIFACTS}/${controller_logs_repo}/${namespace}-${pod}-${container}.txt"
       echo ">>> The dump of Namespace, Pod, Container: ${namespace}, ${pod}, ${container} is located at ${current_output}"
       echo "Namespace, Pod, Container: ${namespace}, ${pod}, ${container}"  >> "${current_output}"
       kubectl logs -n $namespace "${pod}" -c "${container}" >> "${current_output}"
       echo "----------------------------------------------------------"
-      local previous_output="${ARTIFACTS}/k8s.dump-$(basename ${E2E_SCRIPT})-${namespace}-${pod}-${container}-previous.txt"
+      local previous_output="${ARTIFACTS}/${controller_logs_repo}/previous-${namespace}-${pod}-${container}.txt"
       echo ">>> The dump of Namespace, Pod, Container (Previous instance): ${namespace}, ${pod}, ${container} is located at ${previous_output}"
       echo "Namespace, Pod, Container (Previous instance): ${namespace}, ${pod}, ${container}"  >> "${previous_output}"
       kubectl logs -p -n $namespace "${pod}" -c "${container}" >> "${previous_output}"
