189bcadca988260164decd095454c5a80021598e
Al Zargarpur <58704856+zargarpur@users.noreply.github.com>
2020-06-26 19:09:28 -0700

    Limit the values of event_type in Broker metrics (#1352)
    * Limit the values of event_type & filter_type in Broker & Trigger metrics
    
    With custom events, these values can be any string which impacts the
    overall cardinality of the metrics.
    
    We're instead going to limit these values to be any GCP event type, as
    defined by the "com.google.cloud" prefix. And include some values used
    in some E2E tests. All other event types will default to being labeled
    as "custom"
    
    Trigger metrics will continue to label filter type as "any" if there is
    no filter type defined.
    
    * PR Fixes
    
    * Can't use code under test/ ?
    
    * Undo trigger
    
    * Empty line added

--- pkg/broker/ingress/handler_test.go
+++ pkg/broker/ingress/handler_test.go
@@ -59,7 +59,7 @@ const (
 
 	traceID = "4bf92f3577b34da6a3ce929d0e0e4736"
 
-	eventType = "test-event-type"
+	eventType = "google.cloud.scheduler.job.v1.executed"
 	pod       = "testpod"
 	container = "testcontainer"
 )
