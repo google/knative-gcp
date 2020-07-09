fbe7f26e3f583c31b3d68e90710a1321e4042ac4
Danying Gu <danyinggu@google.com>
2020-07-09 16:46:06 -0400

    Promote duck to v1

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -52,7 +52,7 @@ chmod +x "${CODEGEN_PKG}"/generate-groups.sh
 # Only deepcopy the Duck types, as they are not real resources.
 "${CODEGEN_PKG}"/generate-groups.sh "deepcopy" \
   github.com/google/knative-gcp/pkg/client github.com/google/knative-gcp/pkg/apis \
-  "duck:v1alpha1 duck:v1beta1" \
+  "duck:v1alpha1 duck:v1beta1 duck:v1" \
   --go-header-file "${REPO_ROOT_DIR}"/hack/boilerplate/boilerplate.go.txt
 
 # generate the code with:
@@ -68,7 +68,7 @@ chmod +x "${CODEGEN_PKG}"/generate-groups.sh
 chmod +x "${KNATIVE_CODEGEN_PKG}"/hack/generate-knative.sh
 "${KNATIVE_CODEGEN_PKG}"/hack/generate-knative.sh "injection" \
   github.com/google/knative-gcp/pkg/client github.com/google/knative-gcp/pkg/apis \
-  "messaging:v1alpha1 messaging:v1beta1 events:v1alpha1 events:v1beta1 duck:v1alpha1 duck:v1beta1 broker:v1beta1 intevents:v1alpha1 intevents:v1beta1" \
+  "messaging:v1alpha1 messaging:v1beta1 events:v1alpha1 events:v1beta1 duck:v1alpha1 duck:v1beta1 duck:v1 broker:v1beta1 intevents:v1alpha1 intevents:v1beta1" \
   --go-header-file "${REPO_ROOT_DIR}"/hack/boilerplate/boilerplate.go.txt
 
 # Deep copy configs.
