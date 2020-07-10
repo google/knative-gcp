2905fdf998e3f6c26d599c1a0e20556b1d6ef881
Grace Gao <52978759+grac3gao@users.noreply.github.com>
2020-07-09 23:41:18 -0700

    Reduce flakiness caused by sending events error (#1415)
    * update test
    
    * sending events error
    
    * sending events error
    
    * update comment
    
    * comments
    
    * comments
    
    * update retry function
    
    * update retry error
    
    * comments
    
    * comments
    
    * comments

--- test/e2e/test_gcp_broker.go
+++ test/e2e/test_gcp_broker.go
@@ -56,7 +56,7 @@ func GCPBrokerTestImpl(t *testing.T, authConfig lib.AuthConfig) {
 	client := lib.Setup(t, true, authConfig.WorkloadIdentity)
 	defer lib.TearDown(client)
 	brokerURL, brokerName := createGCPBroker(client)
-	kngcphelpers.BrokerEventTransformationTestHelper(client, brokerURL, brokerName)
+	kngcphelpers.BrokerEventTransformationTestHelper(client, brokerURL, brokerName, true)
 }
 
 func GCPBrokerMetricsTestImpl(t *testing.T, authConfig lib.AuthConfig) {
@@ -177,5 +177,8 @@ func createGCPBroker(client *lib.Client) (url.URL, string) {
 	if err != nil {
 		client.T.Error(err.Error())
 	}
+	// Avoid propagation delay between the controller reconciles the broker config and
+	// the config being pushed to the configmap volume in the ingress pod.
+	time.Sleep(15 * time.Second)
 	return u, brokerName
 }
