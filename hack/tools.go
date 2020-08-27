30cbaf1ec65f37d0df0828ad531bd237f8e3f827
Danying Gu <danyinggu@google.com>
2020-08-27 11:43:40 -0400

    fix format

--- hack/tools.go
+++ hack/tools.go
@@ -28,9 +28,9 @@ import (
 	_ "knative.dev/eventing/test/test_images/transformevents"
 
 	_ "knative.dev/eventing/test/test_images/performance"
+	_ "knative.dev/pkg/apiextensions/storageversion/cmd/migrate"
 	_ "knative.dev/pkg/test/mako/stub-sidecar"
 	_ "knative.dev/pkg/testutils/clustermanager/perf-tests"
-	_ "knative.dev/pkg/apiextensions/storageversion/cmd/migrate"
 
 	_ "github.com/google/wire/cmd/wire"
 )
