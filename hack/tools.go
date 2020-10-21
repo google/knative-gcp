6bc3d7ff2afc2292373fc04abeb91baf27be621e
Tommy Reddad <tommy.reddad@gmail.com>
2020-10-21 10:11:06 -0400

    Remove deprecated test images.

--- hack/tools.go
+++ hack/tools.go
@@ -25,7 +25,6 @@ import (
 
 	_ "knative.dev/eventing/test/test_images/event-sender"
 	_ "knative.dev/eventing/test/test_images/recordevents"
-	_ "knative.dev/eventing/test/test_images/transformevents"
 
 	_ "knative.dev/eventing/test/test_images/performance"
 	_ "knative.dev/pkg/apiextensions/storageversion/cmd/migrate"
