0f98edcf37c911c5b75f8f171169deb3528d5830
Zhongduo Lin (Jimmy) <zhongduo@google.com>
2020-10-23 16:24:38 -0400

    Add upgrade test (#1854)
    * feat(testing): add upgrade and downgrade e2e test
    
    This is mostly from the knative eventing upgrade test, modified for installing
    different cloud run events version instead. The result verification is done
    differently so that: 1. not receiving events are errors, 2. duplicate events
    are warning in log, but not errors.
    
    * Ignore duplicate event as errors in update test
    
    * resolve comment

---
+++ test/upgrade/prober/configuration.go
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2020 The Knative Authors
+ * Modified work Copyright 2020 Google LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package prober
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"path"
+	"runtime"
+	"text/template"
+
+	"github.com/google/knative-gcp/pkg/apis/broker/v1beta1"
+	"github.com/wavesoftware/go-ensure"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	eventingv1beta1 "knative.dev/eventing/pkg/apis/eventing/v1beta1"
+	testlib "knative.dev/eventing/test/lib"
+	"knative.dev/eventing/test/lib/duck"
+	"knative.dev/eventing/test/lib/resources"
+	"knative.dev/pkg/apis"
+)
+
+const (
+	configName       = "wathola-config"
+	configMountPoint = "/home/nonroot/.config/wathola"
+	configFilename   = "config.toml"
+	watholaEventNs   = "com.github.cardil.wathola"
+	healthEndpoint   = "/healthz"
+)
+
+var (
+	eventTypes = []string{"step", "finished"}
+	brokerName = "default"
+)
+
+func (p *prober) deployConfiguration() {
+	p.deployBroker()
+	p.deployConfigMap()
+	p.deployTriggers()
+}
+
+func (p *prober) deployBroker() {
+	// Almost all the code in this directory is from knative eventing, but we need
+	// to add this annotation to switch to gcp broker type
+	p.client.CreateBrokerV1Beta1OrFail(brokerName, func(b *eventingv1beta1.Broker) {
+		b.SetAnnotations(map[string]string{
+			"eventing.knative.dev/broker.class": v1beta1.BrokerClass,
+		})
+	})
+}
+
+func (p *prober) fetchBrokerUrl() (*apis.URL, error) {
+	namespace := p.config.Namespace
+	p.log.Debugf("Fetching %s broker URL for ns %s", brokerName, namespace)
+	meta := resources.NewMetaResource(brokerName, p.config.Namespace, testlib.BrokerTypeMeta)
+	err := duck.WaitForResourceReady(p.client.Dynamic, meta)
+	if err != nil {
+		return nil, err
+	}
+	broker, err := p.client.Eventing.EventingV1beta1().Brokers(namespace).Get(context.Background(), brokerName, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+	url := broker.Status.Address.URL
+	p.log.Debugf("%s broker URL for ns %s is %v", brokerName, namespace, url)
+	return url, nil
+}
+
+func (p *prober) deployConfigMap() {
+	name := configName
+	p.log.Infof("Deploying config map: \"%s/%s\"", p.config.Namespace, name)
+	brokerUrl, err := p.fetchBrokerUrl()
+	ensure.NoError(err)
+	configData := p.compileTemplate(configFilename, brokerUrl)
+	p.client.CreateConfigMapOrFail(name, p.config.Namespace, map[string]string{configFilename: configData})
+}
+
+func (p *prober) deployTriggers() {
+	for _, eventType := range eventTypes {
+		name := fmt.Sprintf("wathola-trigger-%v", eventType)
+		fullType := fmt.Sprintf("%v.%v", watholaEventNs, eventType)
+		subscriberOption := resources.WithSubscriberServiceRefForTriggerV1Beta1(receiverName)
+		if p.config.Serving.Use {
+			subscriberOption = resources.WithSubscriberKServiceRefForTrigger(forwarderName)
+		}
+		p.client.CreateTriggerOrFailV1Beta1(name,
+			resources.WithBrokerV1Beta1(brokerName),
+			resources.WithAttributesTriggerFilterV1Beta1(
+				eventingv1beta1.TriggerAnyFilter,
+				fullType,
+				map[string]interface{}{},
+			),
+			subscriberOption)
+	}
+}
+
+func (p *prober) compileTemplate(templateName string, brokerUrl *apis.URL) string {
+	_, filename, _, _ := runtime.Caller(0)
+	templateFilepath := path.Join(path.Dir(filename), templateName)
+	templateBytes, err := ioutil.ReadFile(templateFilepath)
+	ensure.NoError(err)
+	tmpl, err := template.New(templateName).Parse(string(templateBytes))
+	ensure.NoError(err)
+	var buff bytes.Buffer
+	data := struct {
+		*Config
+		BrokerUrl string
+	}{
+		p.config,
+		brokerUrl.String(),
+	}
+	ensure.NoError(tmpl.Execute(&buff, data))
+	return buff.String()
+}
