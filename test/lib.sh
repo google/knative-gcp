c445bd1c1003b692af1a5ec7906ca1c25b406002
Tommy Reddad <tommy.reddad@gmail.com>
2020-10-29 15:52:34 -0400

    Automatically succeed on conformance tests (release-0.15b) (#1873)
    * Automatically succeed on conformance tests (release-0.15b)
    
    * use local version of monitoring
    
    * source the right thing
    
    * use new zipkin endpoint
    
    * Revert monitoring fix
    
    * Override broken test-infra functions
    
    * Can't override readonly variables
    
    * Fix suffix pruning.

--- test/lib.sh
+++ test/lib.sh
@@ -19,11 +19,70 @@
 readonly CLOUD_RUN_EVENTS_CONFIG="config/"
 readonly CLOUD_RUN_EVENTS_ISTIO_CONFIG="config/istio"
 
+# Returns whether the current branch is a release branch.
+# Overrides test-infra's is_release_branch to allow for an optional letter suffix.
+function is_release_branch() {
+  [[ $(current_branch) =~ ^release-[0-9\.]+[a-z]?$ ]]
+}
+
+# Returns the URL to the latest manifest for the given Knative project.
+# Overrides test-infra's get_latest_knative_yaml_source to prune optional letter suffix
+# for release branches.
+# Parameters: $1 - repository name of the given project
+#             $2 - name of the yaml file, without extension
+function get_latest_knative_yaml_source() {
+  local repo_name="$1"
+  local yaml_name="$2"
+  # If it's a release branch, the yaml source URL should point to a specific version.
+  if is_release_branch; then
+    # Extract the release major&minor version from the branch name.
+    local branch_name="$(current_branch)"
+    local major_minor_suffix="${branch_name##release-}"
+    local major_minor="${major_minor_suffix%%[a-z]}"
+    # Find the latest release manifest with the same major&minor version.
+    local yaml_source_path="$(
+      gsutil ls gs://knative-releases/${repo_name}/previous/v${major_minor}.*/${yaml_name}.yaml 2> /dev/null \
+      | sort \
+      | tail -n 1 \
+      | cut -b6-)"
+    # The version does exist, return it.
+    if [[ -n "${yaml_source_path}" ]]; then
+      echo "https://storage.googleapis.com/${yaml_source_path}"
+      return
+    fi
+    # Otherwise, fall back to nightly.
+  fi
+  echo "https://storage.googleapis.com/knative-nightly/${repo_name}/latest/${yaml_name}.yaml"
+}
+
+# Get the latest YAML sources again to override the test-infra sources which
+# fail to recognize release-0.15b as a release branch. Must rename the variables
+# since the originals are readonly.
+readonly OVERRIDE_KNATIVE_SERVING_RELEASE_CRDS="$(get_latest_knative_yaml_source "serving" "serving-crds")"
+readonly OVERRIDE_KNATIVE_SERVING_RELEASE_CORE="$(get_latest_knative_yaml_source "serving" "serving-core")"
+readonly OVERRIDE_KNATIVE_NET_ISTIO_RELEASE="$(get_latest_knative_yaml_source "net-istio" "net-istio")"
+readonly OVERRIDE_KNATIVE_EVENTING_RELEASE="$(get_latest_knative_yaml_source "eventing" "eventing")"
+readonly OVERRIDE_KNATIVE_MONITORING_RELEASE="$(get_latest_knative_yaml_source "serving" "monitoring")"
+
+# Install the latest stable Knative Eventing in the current cluster.
+# Overrides test-infra's start_latest_knative_eventing to use the special
+# overridden variables.
+function start_latest_knative_eventing() {
+  start_knative_eventing "${OVERRIDE_KNATIVE_EVENTING_RELEASE}"
+}
+
+# Install the latest stable Knative Serving in the current cluster.
+# Overrides test-infra's start_latest_knative_serving to use the special
+# overridden variables.
+function start_latest_knative_serving() {
+  start_knative_serving "${OVERRIDE_KNATIVE_SERVING_RELEASE_CRDS}" "${OVERRIDE_KNATIVE_SERVING_RELEASE_CORE}" "${OVERRIDE_KNATIVE_NET_ISTIO_RELEASE}"
+}
+
 # Install all required components for running knative-gcp.
 function start_knative_gcp() {
   start_latest_knative_serving || return 1
   start_latest_knative_eventing || return 1
-  start_knative_monitoring "$KNATIVE_MONITORING_RELEASE" || return 1
+  start_knative_monitoring "$OVERRIDE_KNATIVE_MONITORING_RELEASE" || return 1
   cloud_run_events_setup || return 1
   istio_patch || return 1
 }
