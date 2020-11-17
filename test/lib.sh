1eb4c057d93f083e131a518a046d1b08e1b15513
danyinggu <danyinggu@google.com>
2020-11-17 11:58:46 -0500

    A workaround to fix the config-imc-event-dispatcher issue

--- test/lib.sh
+++ test/lib.sh
@@ -40,7 +40,9 @@ function start_knative_gcp_monitoring() {
 # Install all required components for running knative-gcp.
 function start_knative_gcp() {
   start_latest_knative_serving || return 1
-  start_latest_knative_eventing || return 1
+  #TODO: Restore the below line after https://github.com/knative/eventing/issues/3244 is fixed
+  #start_latest_knative_eventing || return 1
+  start_latest_knative_eventing || start_latest_knative_eventing || return 1
   start_knative_gcp_monitoring "$KNATIVE_GCP_MONITORING_YAML" || return 1
   cloud_run_events_setup $@ || return 1
   istio_patch || return 1
