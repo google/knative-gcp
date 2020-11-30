46c11fe7498b8eb7c794fcee8f3b102bc4069634
Tommy Reddad <reddad@google.com>
2020-11-30 15:34:27 -0500

    fix boilerplates

--- test/test_images/probe_helper/main.go
+++ test/test_images/probe_helper/main.go
@@ -1,15 +1,35 @@
 /*
 Copyright 2020 Google LLC
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+	"go.uber.org/zap"
+
+	"knative.dev/pkg/logging"
+	"knative.dev/pkg/signals"
+
+	pkgutils "github.com/google/knative-gcp/pkg/utils"
+	"github.com/google/knative-gcp/test/test_images/probe_helper/probe"
+	"github.com/google/knative-gcp/test/test_images/probe_helper/utils"
+)
 
 /*
 
@@ -77,23 +97,6 @@ The Probe Helper can handle multiple different types of probes.
 	and waits to observe its creation having been logged by a CloudAuditLogsSource.
 
 */
-
-package main
-
-import (
-	"fmt"
-	"time"
-
-	"github.com/kelseyhightower/envconfig"
-	"go.uber.org/zap"
-
-	"knative.dev/pkg/logging"
-	"knative.dev/pkg/signals"
-
-	pkgutils "github.com/google/knative-gcp/pkg/utils"
-	"github.com/google/knative-gcp/test/test_images/probe_helper/probe"
-	"github.com/google/knative-gcp/test/test_images/probe_helper/utils"
-)
 
 type envConfig struct {
 	// Environment variable containing the base URL for the brokercell ingress, used in the broker e2e delivery probe
