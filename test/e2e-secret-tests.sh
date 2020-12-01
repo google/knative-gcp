8d7b1f03be2289754d9e246a2855c1b8a1a8531a
Jimmy Lin <zhongduo@google.com>
2020-11-30 20:38:32 -0500

    Remove duplicate source for hack/libaray.sh

--- test/e2e-secret-tests.sh
+++ test/e2e-secret-tests.sh
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-source $(dirname "${BASH_SOURCE[0]}")/../vendor/knative.dev/hack/e2e-tests.sh
-
 source $(dirname "${BASH_SOURCE[0]}")/lib.sh
 
 source $(dirname "${BASH_SOURCE[0]}")/../hack/lib.sh
--- test/e2e-wi-tests.sh
+++ test/e2e-wi-tests.sh
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-source $(dirname "${BASH_SOURCE[0]}")/../vendor/knative.dev/hack/e2e-tests.sh
 
 source $(dirname "${BASH_SOURCE[0]}")/lib.sh
 
